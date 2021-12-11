25a0c2f39890fd7c5dc29ac5f1cbd1dfe7586e17
Thomas Renger <thomas.renger@gmail.com>
2021-12-11 16:06:01 +0100

    chore: clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,6 @@ interface Hero {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   readonly data: Hero[] = [{
