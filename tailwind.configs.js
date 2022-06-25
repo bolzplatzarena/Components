4381102f50e21b59980a5bfdc5a348e9e40170c5
Thomas Renger <thomas.renger@gmail.com>
2022-06-25 23:53:12 +0200

    chore: bump packages

---
+++ tailwind.configs.js
@@ -0,0 +1,11 @@
+module.exports = {
+  prefix: 'tm,wbaaad-',
+  content: [
+    "./src/**/*.{html,ts}",
+    "./projects/**/*.{html,ts}",
+  ],
+  important: true,
+  corePlugins: {
+    preflight: false,
+  }
+}
