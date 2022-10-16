615aaa4519a79f3c49e240e79f0b7e73170678e9
Thomas Renger <thomas.renger@gmail.com>
2022-10-16 11:56:44 +0200

    refactor: rebuild the menu

--- projects/components/src/lib/components.module.ts
+++ projects/components/src/lib/components.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSortModule } from '@angular/material/sort';
@@ -8,6 +10,7 @@ import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faAddressCard } from '@fortawesome/free-solid-svg-icons/faAddressCard';
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons/faEllipsisVertical';
 import { faSkullCrossbones } from '@fortawesome/free-solid-svg-icons/faSkullCrossbones';
 import { TranslateModule } from '@ngx-translate/core';
 import { TableComponent } from './components/table/table.component';
@@ -31,6 +34,8 @@ import { InnerTableComponent } from './components/table/inner-table/inner-table.
     MatSortModule,
     MatTableModule,
     TranslateModule,
+    MatMenuModule,
+    MatIconModule,
   ],
   exports: [
     EnumKeyPipe,
@@ -42,5 +47,6 @@ export class ComponentsModule {
   constructor(library: FaIconLibrary) {
     library.addIcons(faAddressCard);
     library.addIcons(faSkullCrossbones);
+    library.addIcons(faEllipsisVertical)
   }
 }
