e30c7a1e579d9935624a71009ff7c146e4c0ca72
Thomas Renger <thomas.renger@gmail.com>
2023-12-23 17:38:48 +0100

    feat: colors

--- projects/components/src/lib/components/button/button.component.ts
+++ projects/components/src/lib/components/button/button.component.ts
@@ -1,3 +1,4 @@
+import { NgTemplateOutlet } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -8,10 +9,13 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
   imports: [
     MatButtonModule,
     MatProgressSpinnerModule,
+    NgTemplateOutlet,
   ],
   templateUrl: './button.component.html',
 })
 export class ButtonComponent {
+  @Input({ required: true }) type!: 'raised' | 'stroked';
+  @Input({ required: false }) color: 'primary' | 'accent' | 'warn' = 'primary';
   @Input({ required: true }) text!: string;
   @Input({ required: true }) loading = false;
 }
