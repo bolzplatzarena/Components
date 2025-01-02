c0986a66c9bf0fc124b742dc419e19712b63dafa
Thomas Renger <thomas.renger@gmail.com>
2025-01-02 12:26:48 +0100

    feat: support at least the text node

--- projects/components/src/lib/components/strapi-renderer/strapi-renderer.component.ts
+++ projects/components/src/lib/components/strapi-renderer/strapi-renderer.component.ts
@@ -1,10 +1,15 @@
-import {Component} from '@angular/core';
+import {Component, input} from '@angular/core';
+import {RootNode} from "../../models/strapi-content.model";
+import {InnerStrapiRendererComponent} from "./inner-strapi-renderer/inner-strapi-renderer.component";
 
 @Component({
   selector: 'bpa-strapi-renderer',
-  imports: [],
+  imports: [
+    InnerStrapiRendererComponent
+  ],
   templateUrl: './strapi-renderer.component.html',
   styleUrl: './strapi-renderer.component.css'
 })
 export class StrapiRendererComponent {
+  readonly content = input.required<RootNode[]>()
 }
--- projects/components/src/public-api-strapi.ts
+++ projects/components/src/public-api-strapi.ts
@@ -1,1 +1,2 @@
 export {StrapiRendererComponent} from './lib/components/strapi-renderer/strapi-renderer.component';
+export {RootNode} from './lib/models/strapi-content.model';
